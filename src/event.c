54fda7a5165ae18b185f342162c7d22272a260d0
Thomas BEGUIN <tbeguin@e3r11p5.42.fr>
2019-03-27 18:33:31 +0100

    mise a la norme fichier event.c

--- src/event.c
+++ src/event.c
@@ -6,12 +6,11 @@
 /*   By: tbeguin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/14 18:31:01 by tbeguin           #+#    #+#             */
-/*   Updated: 2019/03/27 00:04:26 by tbeguin          ###   ########.fr       */
+/*   Updated: 2019/03/27 18:28:02 by tbeguin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/fdf.h"
-
 
 #define BLUE 0x0A5FE8
 #define WHITE 0xFFFFFF
@@ -29,18 +28,12 @@ void		ft_render(t_mlx *mlx_all)
 			mlx_all->win->img_ptr, 0, 0);
 }
 
-int 		ft_key_hook(int key, void *para)
+static void	ft_proj_event(int key, t_mlx *mlx_all)
 {
-	t_mlx *mlx_all;
+	if (key == 34)
+		ft_set_iso(mlx_all, 'i');
 
-	mlx_all = (t_mlx *)para;
-	if (key == 53) /* key ==  'esc'*/
-	{
-		mlx_destroy_window(mlx_all->mlx_ptr, mlx_all->win->win_ptr);
-		exit(0);
-		return (1);
-	}
-	else if (key == 15) /* Key == 'r' */
+	else if (key == 15)
 	{
 		if (mlx_all->cam->proj == 'i')
 			ft_set_iso(mlx_all, 'r');
@@ -48,117 +41,97 @@ int 		ft_key_hook(int key, void *para)
 			ft_set_para(mlx_all, 'r');
 		ft_render(mlx_all);
 	}
-	else if (key == 12) /* key == 'q' */
-	{
-		mlx_all->cam->angle /= 1.2;
-		ft_render(mlx_all);
-	}
-	else if (key == 14) /* key == 'e' */
-	{
-		mlx_all->cam->angle *= 1.2;
-		ft_render(mlx_all);
-	}
-	else if (key == 78) /* key == '-' pav num */
-	{
-		mlx_all->cam->di_x /= 1.2;
-		mlx_all->cam->di_y /= 1.2;
-		mlx_all->cam->di_z /= 1.2;
-		ft_render(mlx_all);
-	}
-	else if (key == 69) /* key == '+' pav num */
-	{
-		mlx_all->cam->di_x *= 1.2;
-		mlx_all->cam->di_y *= 1.2;
-		mlx_all->cam->di_z *= 1.2;
-		ft_render(mlx_all);
-	}
-	else if (key == 34) /* key == 'i' */
-	{
-		ft_set_iso(mlx_all, 'i');
-		ft_render(mlx_all);
-	}
-	else if (key == 126) /* key == 'arrow up' */
-	{
-		mlx_all->cam->up_down -= 10;
-		ft_render(mlx_all);
-	}
-	else if (key == 125) /* key == 'arrow down' */
-	{
-		mlx_all->cam->up_down += 10;
-		ft_render(mlx_all);
-	}
-	else if (key == 123) /* key == 'arrow left' */
-	{
-		mlx_all->cam->left_right -= 10;
-		ft_render(mlx_all);
-	}
-	else if (key == 124) /* key == 'arrow right' */
-	{
-		mlx_all->cam->left_right += 10;
-		ft_render(mlx_all);
-	}
-	else if (key == 116) /* key == 'page up' */
-	{
-		mlx_all->cam->di_z += 1;
-		ft_render(mlx_all);
-	}
-	else if (key == 121) /* key == 'page down' */
-	{
-		mlx_all->cam->di_z -= 1;
-		ft_render(mlx_all);
-	}
-	else if (key == 86) /* key == '4' pav num */
-	{
-		ft_x_rotation(mlx_all, 0.1);
-		ft_render(mlx_all);
-	}
-	else if (key == 88) /* key == '6' pav num */
-	{
-		ft_x_rotation(mlx_all, -0.1);
-		ft_render(mlx_all);
-	}
-	else if (key == 91) /* key == '8' pav num */
-	{
-		ft_y_rotation(mlx_all, 0.1);
-		ft_render(mlx_all);
-	}
-	else if (key == 84) /* key == '2' pav num */
-	{
-		ft_y_rotation(mlx_all, -0.1);
-		ft_render(mlx_all);
-	}
-	else if (key == 89) /* key == '7' pav num */
-	{
-		ft_z_rotation(mlx_all, 0.1);
-		ft_render(mlx_all);
-	}
-	else if (key == 85) /* key == '3' pav num */
-	{
-		ft_z_rotation(mlx_all, -0.1);
-		ft_render(mlx_all);
-	}
-	else if (key == 35) /* key ==  'p'*/
+	else if (key == 35)
 	{
 		if (mlx_all->cam->proj != 'p')
 			ft_set_para(mlx_all, 'p');
 		ft_render(mlx_all);
 	}
-	else if (key == 43 && mlx_all->cam->opacity < 4026531840)
+	ft_render(mlx_all);
+}
+
+static void	ft_mouv_event(int key, t_mlx *mlx_all)
+{
+	if (key == 78)
 	{
+		mlx_all->cam->di_x /= 1.2;
+		mlx_all->cam->di_y /= 1.2;
+		mlx_all->cam->di_z /= 1.2;
+	}
+	else if (key == 69)
+	{
+		mlx_all->cam->di_x *= 1.2;
+		mlx_all->cam->di_y *= 1.2;
+		mlx_all->cam->di_z *= 1.2;
+	}
+	else if (key == 126)
+		mlx_all->cam->up_down -= 10;
+	else if (key == 125)
+		mlx_all->cam->up_down += 10;
+	else if (key == 123)
+		mlx_all->cam->left_right -= 10;
+	else if (key == 124)
+		mlx_all->cam->left_right += 10;
+	else if (key == 116)
+		mlx_all->cam->di_z += 1;
+	else if (key == 121)
+		mlx_all->cam->di_z -= 1;
+	ft_render(mlx_all);
+}
+
+static void ft_rot_event(int key, t_mlx *mlx_all)
+{
+	if (key == 12)
+		mlx_all->cam->angle /= 1.2;
+	else if (key == 86)
+		ft_x_rotation(mlx_all, 0.1);
+	else if (key == 88)
+		ft_x_rotation(mlx_all, -0.1);
+	else if (key == 91)
+		ft_y_rotation(mlx_all, 0.1);
+	else if (key == 84)
+		ft_y_rotation(mlx_all, -0.1);
+	else if (key == 89)
+		ft_z_rotation(mlx_all, 0.1);
+	else if (key == 85)
+		ft_z_rotation(mlx_all, -0.1);
+	else if (key == 12)
+		mlx_all->cam->angle /= 1.2;
+	else if (key == 14)
+		mlx_all->cam->angle *= 1.2;
+	ft_render(mlx_all);
+}
+
+static void	ft_color_event(int key, t_mlx *mlx_all)
+{
+	if (key == 43 && mlx_all->cam->opacity < 0xf0000000)
 		mlx_all->cam->opacity += 0x10000000;
-		ft_render(mlx_all);
-	}
 	else if (key == 47 && mlx_all->cam->opacity > 0)
-	{
 		mlx_all->cam->opacity -= 0x10000000;
-		ft_render(mlx_all);
+	ft_render(mlx_all);
+}
+
+int 		ft_key_hook(int key, void *para)
+{
+	t_mlx *mlx_all;
+
+	mlx_all = (t_mlx *)para;
+	if (key == 53)
+	{
+		mlx_destroy_window(mlx_all->mlx_ptr, mlx_all->win->win_ptr);
+		exit(0);
+		return (0);
 	}
-	if (key == 47 || key == 43)
-		printf("opacity = %u\n", mlx_all->cam->opacity);
-	ft_putstr("----------------------key----------------------\n");
-	ft_putnbr(key);
-	ft_putstr("\n");
-	ft_putstr("-----------------------------------------------\n");
+	else if (key == 15 || key == 34 || key == 35)
+		ft_proj_event(key, mlx_all);
+	else if (key == 78 || key == 69 || key == 116 || key == 121
+			|| key == 123 || key == 124|| key == 125 || key == 126)
+		ft_mouv_event(key, mlx_all);
+	else if (key == 43 || key == 47)
+		ft_color_event(key, mlx_all);
+	else if (key == 12 || key == 14 || key == 84 || key == 85
+			|| key == 86 || key == 88 || key == 89 || key == 91)
+		ft_rot_event(key, mlx_all);
 	return (0);
 }
 
@@ -167,14 +140,6 @@ int		ft_mouse_hook(int button, int x, int y, void *para)
 	t_mlx		*mlx_all;
 
 	mlx_all = (t_mlx *)para;
-	ft_putstr("----------------------mouse--------------------\n");
-	ft_putnbr(button);
-	ft_putstr("\n");
-	ft_putnbr(x);
-	ft_putstr("\n");
-	ft_putnbr(y);
-	ft_putstr("\n");
-	ft_putstr("-----------------------------------------------\n");
 	if (button == 1)
 	{
 		if (mlx_all->win->ligne == 1)
