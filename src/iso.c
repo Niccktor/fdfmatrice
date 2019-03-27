7266345ded924a2e17802d99920a5cbaafebb7a0
Thomas BEGUIN <tbeguin@e3r11p5.42.fr>
2019-03-27 19:22:40 +0100

    Mise a la norme

--- src/iso.c
+++ src/iso.c
@@ -6,53 +6,49 @@
 /*   By: tbeguin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/20 21:54:51 by tbeguin           #+#    #+#             */
-/*   Updated: 2019/03/26 16:44:12 by tbeguin          ###   ########.fr       */
+/*   Updated: 2019/03/27 19:22:10 by tbeguin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/fdf.h"
 #include <math.h>
+
 /*
-	y
-	|
-	|
-	|
-	|
-	---------------------x
-	i = y;
-	j = x;
-
-	*x = (previous_x - previous_y) * cos(mlx_all->cam->angle);
-	*y = -z + (previous_x + previous_y) * sin(mlx_all->cam->angle);
-
+** 	y
+**	|
+**	|
+**	|
+**	|
+**	---------------------x
+**	i = y;
+**	j = x;
+**
+**	*x = (previous_x - previous_y) * cos(mlx_all->cam->angle);
+**	*y = -z + (previous_x + previous_y) * sin(mlx_all->cam->angle);
 */
+
 static void		ft_iso_right(t_mlx *mlx_all, int i, int j)
 {
 	int x;
 	int y;
-	int x_add;
-	int y_add;
-
-	x_add = mlx_all->cam->left_right;
-	y_add = mlx_all->cam->up_down;
 
 	mlx_all->win->x_ligne = (mlx_all->map->map[i][j]->x * mlx_all->cam->di_x
 		- mlx_all->map->map[i][j]->y * mlx_all->cam->di_y)
-		* cos(mlx_all->cam->angle) + x_add;
+		* cos(mlx_all->cam->angle) + mlx_all->cam->left_right;
 	mlx_all->win->y_ligne = -(mlx_all->map->map[i][j]->z * mlx_all->cam->di_z)
 		+ (mlx_all->map->map[i][j]->x * mlx_all->cam->di_x
 		+ mlx_all->map->map[i][j]->y * mlx_all->cam->di_y)
-		* sin(mlx_all->cam->angle) + y_add;
+		* sin(mlx_all->cam->angle) + mlx_all->cam->up_down;
 	if (j + 1 < mlx_all->map->len)
 	{
 		j++;
 		x = (mlx_all->map->map[i][j]->x * mlx_all->cam->di_x
 			- mlx_all->map->map[i][j]->y * mlx_all->cam->di_y)
-			* cos(mlx_all->cam->angle) + x_add;
+			* cos(mlx_all->cam->angle) + mlx_all->cam->left_right;
 		y = -(mlx_all->map->map[i][j]->z * mlx_all->cam->di_z)
 			+ (mlx_all->map->map[i][j]->x * mlx_all->cam->di_x
 			+ mlx_all->map->map[i][j]->y * mlx_all->cam->di_y)
-			* sin(mlx_all->cam->angle) + y_add;
+			* sin(mlx_all->cam->angle) + mlx_all->cam->up_down;
 		ft_draw_ligne(mlx_all, x, y, mlx_all->map->map[i][j - 1]->color);
 	}
 }
@@ -61,19 +57,15 @@ static void		ft_iso_down(t_mlx *mlx_all, int i, int j)
 {
 	int x;
 	int y;
-	int x_add;
-	int y_add;
 
 	i++;
-	x_add = mlx_all->cam->left_right;
-	y_add = mlx_all->cam->up_down;
 	x = (mlx_all->map->map[i][j]->x * mlx_all->cam->di_x
 		- mlx_all->map->map[i][j]->y * mlx_all->cam->di_y)
-		* cos(mlx_all->cam->angle) + x_add;
+		* cos(mlx_all->cam->angle) + mlx_all->cam->left_right;
 	y = -(mlx_all->map->map[i][j]->z * mlx_all->cam->di_z)
 		+ (mlx_all->map->map[i][j]->x * mlx_all->cam->di_x
 		+ mlx_all->map->map[i][j]->y * mlx_all->cam->di_y)
-		* sin(mlx_all->cam->angle) + y_add;
+		* sin(mlx_all->cam->angle) + mlx_all->cam->up_down;
 	ft_draw_ligne(mlx_all, x, y, mlx_all->map->map[i - 1][j]->color);
 }
 
@@ -83,13 +75,6 @@ void			ft_iso(t_mlx *mlx_all)
 	int j;
 
 	i = 0;
-	ft_putstr("\ndi_x = ");
-	ft_putnbr(mlx_all->cam->di_x);
-	ft_putstr(" di_y = ");
-	ft_putnbr(mlx_all->cam->di_y);
-	ft_putstr(" di_z = ");
-	ft_putnbr(mlx_all->cam->di_z);
-	ft_putstr("\n");
 	while (i < mlx_all->map->height)
 	{
 		j = 0;
@@ -104,7 +89,7 @@ void			ft_iso(t_mlx *mlx_all)
 	}
 }
 
-void		ft_set_iso(t_mlx *mlx_all, char para)
+void			ft_set_iso(t_mlx *mlx_all, char para)
 {
 	if (mlx_all->cam->proj != 'i' || para == 'r')
 	{
@@ -114,8 +99,9 @@ void		ft_set_iso(t_mlx *mlx_all, char para)
 		mlx_all->cam->di_z = (mlx_all->win->width / mlx_all->map->len) / 5;
 		mlx_all->cam->up_down = (mlx_all->map->height * mlx_all->cam->di_y
 				+ mlx_all->map->len * mlx_all->cam->di_x) / 4;
-		mlx_all->cam->left_right = 	((mlx_all->map->height / 2 - mlx_all->map->len / 2)
-			* mlx_all->cam->di_y) + mlx_all->win->width / 2;;
+		mlx_all->cam->left_right = ((mlx_all->map->height / 2
+				- mlx_all->map->len / 2)
+				* mlx_all->cam->di_y) + mlx_all->win->width / 2;
 		mlx_all->cam->angle = 0.523599;
 	}
 }
