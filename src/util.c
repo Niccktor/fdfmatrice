8dd7b152c39d42b77e5787683b28f48bb11349d3
Thomas BEGUIN <tbeguin@e3r11p5.42.fr>
2019-03-27 19:31:12 +0100

    Mise a la norme

--- src/util.c
+++ src/util.c
@@ -6,7 +6,7 @@
 /*   By: tbeguin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/14 19:30:51 by tbeguin           #+#    #+#             */
-/*   Updated: 2019/03/27 19:23:46 by tbeguin          ###   ########.fr       */
+/*   Updated: 2019/03/27 19:27:56 by msaubin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,9 +52,8 @@ t_mlx		*ft_new_win(t_mlx *mlx_all, char *s, int width, int height)
 	mlx_all->win->win_ptr = mlx_new_window(mlx_all->mlx_ptr,
 			mlx_all->win->width, mlx_all->win->height, mlx_all->win->name);
 	mlx_all->win->img_ptr = mlx_new_image(mlx_all->mlx_ptr, width, height);
-	mlx_all->win->img_str =
-		(unsigned int *)mlx_get_data_addr(mlx_all->win->img_ptr,
-				&bpp, &s_l, &endian);
+	mlx_all->win->img_str = (unsigned int *)
+		mlx_get_data_addr(mlx_all->win->img_ptr, &bpp, &s_l, &endian);
 	mlx_all->win->ligne = 0;
 	mlx_all->win->x_ligne = 0;
 	mlx_all->win->y_ligne = 0;
